f649ce2dc934151b63d968cfdf5a5a9b08bd134e
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2023-12-11 20:37:26 +0100

    wrote a new function to post data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ export default function App() {
     const [loading, setLoading] = useState(false)
     const [posts, setPosts] = useState([])
     const [error, setError] = useState(null)
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
 
     const FetchDataComponent = () => {
         setLoading(true)
@@ -27,13 +29,37 @@ export default function App() {
         FetchDataComponent()
     }, [])
 
-    function fetchData() {
-
+    function postData(e) {
+        e.preventDefault();
+        axios.post(BASE_URL, { name: name, password: password })
+            .then((response) => {
+                setName(response.data.name)
+            })
+            .catch((error) => {
+                console.error('Error Posting Data!', error)
+                setError("An error occurred while Posting data. Please try again.")
+            })
+            .finally(() => {
+                setName(''); setPassword('');
+            })
+        console.log('Name: ', name)
+        console.log('Password: ', password)
     }
 
     return (
         <>
-            <button onClick={fetchData}>Refresh Data</button>
+            <form onSubmit={postData}>
+                <label htmlFor="nameFIeld">
+                    <input type="text" id="nameFIeld" value={name} onChange={(e) => setName(e.target.value)} />
+                    Name
+                </label>
+                <label htmlFor="password">
+                    <input type="text" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    Password
+                </label>
+                <button type="submit">Submit</button>
+                {error && <p>{error}</p>}
+            </form>
 
             {loading && <p style={{ color: 'blue' }}>Loading ...</p>}
             {error && <p style={{ color: 'red' }}>{error}</p>}
